16f379ebb67d6286593ecfd3781888a63509057c
Yadav Nishit <valoroustech@gmail.com>
2021-12-30 17:17:51 +0530

    minor issue fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import Post from './pages/post';
+// import { BrowserRouter as Router, Route} from "react-router-dom";
+// import Post from './pages/post';
 import PostList from './components/postlist'
 function App() {
   return (
